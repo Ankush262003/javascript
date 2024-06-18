d647d804ee9d264e7f3bacd1db7affc3456a7f86
Ankush262003 <156702359+Ankush262003@users.noreply.github.com>
2024-06-18 13:06:09 +0530

    Delete Problems directory

--- Problems/1_staireCase.js
+++
@@ -1,5 +0,0 @@
-const n = 10;
-const space = " ";
-for (let i = 1; i <= n; i++) {
-  console.log(" ".repeat(n - i) + "#".repeat(i));
-}
--- Problems/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-for(let value of nums){
-
-    // nested loop for for ittrate over each element of array
-    console.log(value)
-}
